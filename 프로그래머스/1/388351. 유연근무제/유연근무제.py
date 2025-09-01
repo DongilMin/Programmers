efe79052341f36548ff214511491d9cc68c866a4
DongilMin <tasabi0826@gmail.com>
2025-09-01 21:01:53 +0900

    [level 1] Title: 유연근무제, Time: 2.84 ms, Memory: 9.64 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.py
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -11,33 +11,17 @@ def time_to_minutes(t):
 
 def solution(schedules, timelogs, startday):
     answer = 0
-    
-    # timelogs에서 한 사람(person_logs)의 기록을 순서대로 가져옴
-    # i는 그 사람의 인덱스
-    for i, person_logs in enumerate(timelogs):
-        
-        # 한 사람의 출근 기록(arrived_time)을 하루씩(j) 확인
-        for j, arrived_time in enumerate(person_logs):
-            
-            # 1. (수정) 괄호를 사용하여 올바른 요일 계산
-            # 2. (개선) 변수로 분리하고 'in'을 사용하여 가독성 향상
+    for i, records in enumerate(timelogs):
+        limit_time = time_to_minutes(schedules[i]) + 10
+        for j, record in enumerate(records):
             current_day = (startday + j) % 7
             
-            # 주말(토요일=6, 일요일=0)인 경우 출근 여부를 확인하지 않음
             if current_day in {0, 6}:
                 continue
             
-            # 스케줄 시간 + 10분(지각 허용 시간)보다 늦게 도착했다면
-            schedule_min = time_to_minutes(schedules[i])
-            arrived_min = time_to_minutes(arrived_time)
-
-            if arrived_min > schedule_min + 10:
-                # 3. (개선) 이 사람은 보상 대상이 아니므로 안쪽 for문을 중단
+            record_time = time_to_minutes(record)
+            if record_time > limit_time:
                 break
-        
-        # 4. (개선) for-else 구문 사용
-        # 안쪽 for문이 break로 중단되지 않았다면 (즉, 한번도 지각 안했다면)
-        # else 블록이 실행됨
         else:
             answer += 1
             
