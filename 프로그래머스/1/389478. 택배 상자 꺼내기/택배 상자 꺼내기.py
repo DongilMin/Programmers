6871643a8ac9699ccc8247efbeb906205b11f5dc
DongilMin <tasabi0826@gmail.com>
2025-09-03 15:37:58 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.04 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,10 +1,8 @@
-def solution(n, w, num):
+def solut(n, w, num):
     answer = 0
-    
-    boxes = [[] for _ in range(w)]
-    
-    number = 1
     direction = 1
+    boxes = [[] for _ in range(w)]
+    number = 1
     while number <= n :
         if direction == 1 :
             for col in range(0, w, 1):
@@ -16,11 +14,35 @@ def solution(n, w, num):
                 boxes[col].append(number)
                 number += 1
                 if number > n: break
-                
+        direction *= -1
+        
+    for box in boxes:
+        if num in box:
+            return len(box) - box.index(num)
+    return 0
+
+
+def solution(n, w, num):
+    answer = 0
+    number = 1
+    direction = 1
+    boxes = [[] for _ in range(w)]
+    
+    while number <= n:
+        if direction == 1:
+            for col in range(w):
+                boxes[col].append(number)
+                number += 1
+                if number > n :
+                    break
+        else:
+            for col in range(w - 1, -1, -1):
+                boxes[col].append(number)
+                number += 1
+                if number > n :
+                    break
         direction *= -1
     
     for box in boxes:
         if num in box:
             return len(box) - box.index(num)
-    
-    return 0
