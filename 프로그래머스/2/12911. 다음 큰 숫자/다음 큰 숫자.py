ce1d16fdfa162afab57d8e33601741068dc1e555
DongilMin <tasabi0826@gmail.com>
2026-02-23 18:50:14 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,8 +1,8 @@
 def solution(n):
     next = n + 1
     while True:
-        if bin(n)[2:].count("1") == bin(next)[2:].count("1"):
+        if bin(n)[2:].count('1') == bin(next)[2:].count('1'):
             return next
         next += 1
-
-    return answer
+        
+    return next
