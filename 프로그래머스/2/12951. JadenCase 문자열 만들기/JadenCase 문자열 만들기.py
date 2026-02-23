004aaf2958b0811b1eab078275f4a7ab6ad0141a
DongilMin <tasabi0826@gmail.com>
2026-02-23 18:03:24 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,17 +1,13 @@
 def solution(s):
-    s_lower = s.lower()
-    result = []
-    
-    is_new_word = True
-    
-    for char in s_lower:
+    is_new = True
+    answer = ""
+    for char in s:
         if char == ' ':
-            result.append(char)
-            is_new_word = True
-        elif is_new_word:
-            result.append(char.upper())
-            is_new_word = False
+            answer += char
+            is_new = True
+        elif is_new:
+            answer += char.upper()
+            is_new = False
         else:
-            result.append(char)
-    
-    return "".join(result)
+            answer += char.lower()
+    return answer
