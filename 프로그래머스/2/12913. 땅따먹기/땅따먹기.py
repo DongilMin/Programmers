d26e8962fab44828290facfeda77c84abd9ba353
DongilMin <tasabi0826@gmail.com>
2026-03-07 17:27:25 +0900

    [level 2] Title: 땅따먹기, Time: 216.94 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -3,9 +3,7 @@ def solution(land):
     for i in range(1, len(land)):
 
         for j in range(len(land[0])):
-            prev_row = land[i-1]
+            prev_row = land[i - 1]
             max_from_prev = max(prev_row[:j] + prev_row[j+1:])
-            
             land[i][j] += max_from_prev
-            
     return max(land[-1])
