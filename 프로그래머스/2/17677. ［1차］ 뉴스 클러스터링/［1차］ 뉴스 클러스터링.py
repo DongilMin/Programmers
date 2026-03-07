c202ad9a787ce383fdf879bda303389567596d31
DongilMin <tasabi0826@gmail.com>
2026-03-07 11:30:45 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.76 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,20 +1,17 @@
 from collections import Counter
 
-
 def solution(str1, str2):
     
-    multiset1 = [str1[i:i+2].lower() for i in range(len(str1) - 1) if str1[i:i+2].isalpha()]
-    multiset2 = [str2[i:i+2].lower() for i in range(len(str2) - 1) if str2[i:i+2].isalpha()]
+    m1 = [str1[i:i + 2].lower() for i in range(len(str1) - 1) if str1[i:i + 2].isalpha()]
+    m2 = [str2[i:i + 2].lower() for i in range(len(str2) - 1) if str2[i:i + 2].isalpha()]
     
-    counter1 = Counter(multiset1)
-    counter2 = Counter(multiset2)
+    counter1 = Counter(m1)
+    counter2 = Counter(m2)
     
-    intersection_size = sum((counter1 & counter2).values())
-    union_size = sum((counter1 | counter2).values())
+    i_size = sum((counter1 & counter2).values())
+    u_size = sum((counter1 | counter2).values())
     
-    if union_size == 0:
+    if u_size == 0:
         return 65536
     
-    j = intersection_size / union_size
-    
-    return int(j * 65536)
+    return int (i_size / u_size * 65536)
