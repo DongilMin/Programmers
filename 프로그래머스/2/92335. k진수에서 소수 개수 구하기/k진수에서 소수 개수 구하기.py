282d540c2ae6fb8b5846b0e50c8b6d8fb5ee08a0
DongilMin <tasabi0826@gmail.com>
2026-03-07 12:39:38 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 73.87 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,33 +1,25 @@
 def is_prime(n):
-    # 1은 소수가 아님
     if n < 2:
         return False
-    
-    # 2는 유일한 짝수 소수
-    if n == 2:
+    elif n == 2:
         return True
-    
-    # 짝수는 2를 제외하고 소수가 아님
-    if n % 2 == 0:
+    elif n % 2 ==0:
         return False
-    
-    # 3부터 n의 제곱근까지 홀수만 확인 (훨씬 효율적)
-    # n의 약수는 항상 sqrt(n)을 기준으로 쌍을 이루므로, sqrt(n)까지만 확인하면 충분함
-    i = 3
-    while i * i <= n:
-        if n % i == 0:
-            return False
-        i += 2  # 홀수만 검사
-        
+    else:
+        i = 3
+        while i * i <= n:
+            if n % i == 0:
+                return False
+            i += 2
+            
     return True
 
 def func(n, k):
     result = ""
     while n > 0:
-        result = str(n%k) + result
+        result = str(n % k) + result
         n //= k
     return result
-
 
 def solution(n, k):
     
