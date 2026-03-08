b5231aad085ebc37d50eb8e98c0e9c2ea1c36691
DongilMin <tasabi0826@gmail.com>
2026-03-09 00:35:56 +0900

    [level 2] Title: 소수 찾기, Time: 5.49 ms, Memory: 9.48 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,23 @@
+from itertools import permutations
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def solution(numbers):
+    answer = 0
+    num_set = set()
+    
+    for i in range(1, len(numbers) + 1):
+        for p in permutations(numbers, i):
+            num_set.add(int(''.join(p)))
+            
+    for n in num_set:
+        if is_prime(n):
+            answer += 1
+            
+    return answer
