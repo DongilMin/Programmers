81da85c54b206aeb88f658a11fd5040264e8aa4a
DongilMin <tasabi0826@gmail.com>
2026-03-10 02:05:08 +0900

    [level 2] Title: 미로 탈출, Time: 12.30 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+def BFS(start_x, start_y, maps, destination):
+    rows = len(maps)
+    cols = len(maps[0])
+    visited = [[False] * cols for _ in range(rows)]
+    
+    queue = deque([(start_x, start_y, 0)])
+    visited[start_x][start_y] = True
+    
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
+    while queue:
+        x, y, dist = queue.popleft()
+        
+        if maps[x][y] == destination:
+            return dist, x, y
+            
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            
+            if 0 <= nx < rows and 0 <= ny < cols:
+                if not visited[nx][ny] and maps[nx][ny] != 'X':
+                    visited[nx][ny] = True
+                    queue.append((nx, ny, dist + 1))
+    return -1, -1, -1
+
+def find_char(maps, target):
+    for r in range(len(maps)):
+        for c in range(len(maps[0])):
+            if maps[r][c] == target:
+                return r, c
+    return None
+
+def solution(maps):
+    s_x, s_y = find_char(maps, 'S')
+    
+    dist_to_lever, l_x, l_y = BFS(s_x, s_y, maps, 'L')
+    if dist_to_lever == -1:
+        return -1
+    
+    dist_to_exit, _, _ = BFS(l_x, l_y, maps, 'E')
+    if dist_to_exit == -1:
+        return -1
+    
+    return dist_to_lever + dist_to_exit
