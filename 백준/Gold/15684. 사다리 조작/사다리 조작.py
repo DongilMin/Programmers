c047000c7549c47efd5b28e5647e78b5d1e96ce6
DongilMin <tasabi0826@gmail.com>
2026-04-05 18:08:19 +0900

    [Gold III] Title: 사다리 조작, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.py
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -1,50 +1,48 @@
 n, m, h = map(int, input().split())
-
-board = [[False] * (n + 1) for _ in range(h + 1)]
+maps = [[0] * (n + 1) for _ in range(h + 1)]
 for _ in range(m):
     a, b = map(int, input().split())
-    board[a][b] = True
+    maps[a][b] = True
 
 candidates = []
 for i in range(1, h + 1):
     for j in range(1, n):
-        if not board[i][j] and not board[i][j - 1] and not board[i][j + 1]:
+        if not maps[i][j] and not maps[i][j - 1] and not maps[i][j + 1]:
             candidates.append((i, j))
 
-def get_wrong_count():
+def count_wrong():
     cnt = 0
     for start in range(1, n + 1):
         now = start
         for j in range(1, h + 1):
-            if board[j][now]:
+            if maps[j][now]:
                 now += 1
-            elif board[j][now - 1]:
+            elif maps[j][now - 1]:
                 now -= 1
         if now != start:
             cnt += 1
     return cnt
 
 def dfs(cnt, idx, limit):
-    wrong = get_wrong_count()
-    
+    wrong = count_wrong()
+
     if wrong == 0:
         print(cnt)
         exit()
-        
     if wrong > (limit - cnt) * 2:
         return
-        
+
     if cnt == limit:
         return
 
     for k in range(idx, len(candidates)):
         r, c = candidates[k]
-        if not board[r][c - 1] and not board[r][c + 1]:
-            board[r][c] = True
+        if not maps[r][c - 1] and not maps[r][c + 1]:
+            maps[r][c] = True
             dfs(cnt + 1, k + 1, limit)
-            board[r][c] = False
+            maps[r][c] = False
 
-if get_wrong_count() == 0:
+if count_wrong() == 0:
     print(0)
     exit()
 
