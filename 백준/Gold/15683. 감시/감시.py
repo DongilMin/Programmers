6a2a621ac8a8c78e1822cbc3f7f1cdc3efb674c6
DongilMin <tasabi0826@gmail.com>
2026-04-05 17:51:10 +0900

    [Gold III] Title: 감시, Time: 800 ms, Memory: 35028 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,24 +1,30 @@
-n, m = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(n)]
-cctv = [(board[i][j], i, j) for i in range(n) for j in range(m) if 1 <= board[i][j] <= 5]
+from collections import deque
 
-# 상(0), 우(1), 하(2), 좌(3) - 시계 방향
+n, m = map(int, input().split())
+maps = [list(map(int, input().split())) for _ in range(n)]
+
+cctv = []
+for i in range(n):
+    for j in range(m):
+        if 0 < maps[i][j] < 6:
+            cctv.append((maps[i][j], i, j))
+
+# 시계방향 상 우 하 좌
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
-
 modes = [
     [],
-    [[0], [1], [2], [3]], # 1번: 한 방향
-    [[0, 2], [1, 3]],     # 2번: 상하 / 좌우
-    [[0, 1], [1, 2], [2, 3], [3, 0]], # 3번: 직각
-    [[0, 1, 2], [1, 2, 3], [2, 3, 0], [3, 0, 1]], # 4번: 세 방향
-    [[0, 1, 2, 3]]        # 5번: 네 방향
+    [[0], [1], [2], [3]],
+    [[0, 2], [1, 3]],
+    [[0, 1], [1, 2], [2, 3], [3, 0]],
+    [[0, 1, 2], [1, 2, 3], [2, 3, 0], [3, 0, 1]],
+    [[0, 1, 2, 3]],
 ]
 
 ans = 1e9
 
 def get_score(selected_dirs):
-    temp = [row[:] for row in board]
+    temp = [row[:] for row in maps]
 
     for i in range(len(cctv)):
         c_type, r, c = cctv[i]
@@ -36,15 +42,13 @@ def get_score(selected_dirs):
 
 def dfs(selected_dirs):
     global ans
-    if len(cctv) == len(selected_dirs):
+    if len(selected_dirs) == len(cctv):
         ans = min(ans, get_score(selected_dirs))
         return
 
-    curr_idx = len(selected_dirs)
-    c_type, r, c = cctv[curr_idx]
-
-    for d in modes[c_type]:
-        selected_dirs.append(d)
+    cctv_type, r, c = cctv[len(selected_dirs)]
+    for mode in modes[cctv_type]:
+        selected_dirs.append(mode)
         dfs(selected_dirs)
         selected_dirs.pop()
 
